29f7dab3db55f3bce68c1e2486bc5258e9655194
FOGBOUND-ALPHA\reverie <contact@rw0.pw>
2016-11-26 14:58:49 -0800

    + Added additional testing for clientServer

--- clientServer_2/webroot/client/my.js
+++ clientServer_2/webroot/client/my.js
@@ -5,9 +5,26 @@ var socket = io(domain, { path: filepath + "/socket.io" });
 
 
 // Called to change the text in the relevant h4 element
-function changeText(){
+function ChangeText(){
   var input = document.getElementById("toSend").value;
   console.log("Attempting to send: " + input);
   socket.emit('message-event', { data: input });
   document.getElementById("toChange").innerHTML = "Sent: " + input;
+}
+
+function MockClientConnect(){
+  console.log("Attempting to perform mock client connection");
+  
+    
+  var data = {
+    "api"          : 1,
+    "session-id"   : 0,
+    "client-id"    : 0,
+    "message-type" : "newClientConnect",
+    "timestamp"    : 0,
+    "message"      : ""
+  }
+  
+
+  socket.emit("client-command", data);
 }
